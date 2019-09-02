fe20b114b32c3a94a3e35b38f3fd141cbf8260d0
ClebersonBieleski <clebersonbieleski@gmail.com>
2019-09-02 14:10:05 -0300

    Clean generate classes Entity

--- src/schema/generate.php
+++ src/schema/generate.php
@@ -39,70 +39,13 @@
 	$generator->setNumSpaces(5);
 	$generator->generate($metadata, $dir);
 
-	$namespace = explode('/', $dir);
-	$namespace = 'Entity\\'.$namespace[count($namespace)-2];
-	$assignature="<?php\nnamespace $namespace;\n";
-
-	$addClass="}\n\n 	public ".'$dbTableName'." = '@NameTable';\n\n}";
-
-	$procurar = array("@ORM\\","private",")\n    {","\n{","<?php\n\n\n\n","}\n}");
-	$colocar = array("@","protected","){","{",$assignature,$addClass);
-
 	$types = array('php');
 	$path = new DirectoryIterator($dir);
 
 	$contador=0;
 	foreach ($path as $fileInfo) {
-		$ext = strtolower( $fileInfo->getExtension() );
-		if( in_array( $ext, $types ) ){
-			$arquivo = $dir.$fileInfo->getFilename();
-			if(($fp = fopen($arquivo, "r"))) {
-				$ponteiro = fopen ($arquivo,"r");
-
-				//LÊ O ARQUIVO ATÉ CHEGAR AO FIM
-				while (!feof ($ponteiro)) {
-					$linha = fgets($ponteiro,4096);
-					$tmpLine=$linha;
-
-					if(strpos($tmpLine,'(\\')!==false){
-						$tmp = explode("(",$tmpLine);
-						$tmp = explode("$",$tmp[1]);
-
-						if($n=array_search($tmp[0],$procurar)){
-							$procurar[$n] = $tmp[0];
-							$colocar[$n] = "";
-						}else{
-							$procurar[] = $tmp[0];
-							$colocar[] = "";
-						}
-					}
-					if(strpos($tmpLine,'* @ORM\Table(name="')!==false){
-						$nametable = explode('"', $tmpLine);
-						if($p=array_search('@NameTable',$procurar)){
-							$procurar[$p] = '@NameTable';
-							$colocar[$p] = $nametable[1];
-						}else{
-							$procurar[] = '@NameTable';
-							$colocar[] = $nametable[1];
-						}
-					}
-				}
-			}
-			//Obtem o conteudo do arquivo
-			$obter = file_get_contents($arquivo);
-			$novo = str_replace($procurar, $colocar, $obter);
-
-			$namClass=str_replace(".php", "", $fileInfo->getFilename());
-			if(strpos($novo,$namClass)!==false){
-				$novo = str_replace($namClass."{", $namClass." extends \Repository\AbstractEntity{", $novo);
-			}
-
-			//Grava o novo texto (modificado) no arquivo
-			$gravar = fopen($arquivo, "w");
-			fwrite($gravar, $novo);
-			fclose($gravar);
+		if( in_array( strtolower( $fileInfo->getExtension() ), $types ) ){
 			$contador++;
-
 		}
 	}
 	if($contador>0){
